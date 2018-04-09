e51643300b5005f30b1e915252126a239045c51c
Neitex <neitexplay@yandex.by>
2018-04-09 21:29:49 +0300

    Added some commands

--- main2.js
+++ main2.js
@@ -10,14 +10,32 @@ ntx.login(TOKEN);
 ntx.on('ready', () => {
     console.log(`              Скрипт от Neitex'a`);
     console.log(`           Скрипт работает стабильно` );
-    console.log(` Скрипт подключён к дискорду как ${ntx.user.taп}!`);
+    console.log(` Скрипт подключён к дискорду как ${ntx.user.tag}!`);
   });
 //-----------Скрипт готов к работе-----\\
 
-
 //-----------Блок комманд--------------\\
-{ //
-  ntx.on('message', (message) =>{
 
+{ //---------Отвечает привет-----------\\
+  ntx.on('message', (message) => {
+    if (message == "привет") {
+      if (message.author == ntx.user.tag) {
+        return;
+      } else {
+        message.reply("Привет! :wink::wave:");
+      }
+    }
+  });
+}
+
+{ //---------Провожает из чата---------\\
+  ntx.on('message', (message) => {
+    if (message == "пока") {
+      if (message.author == ntx.user.tag) {
+        return;
+      } else {
+        message.reply("Пока. Ждём тебя снова :wink:");
+      }
+    }
   });
 }
