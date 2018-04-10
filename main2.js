d1effc71ea6403ca88cdc6fa4b84e9aa568fbfc7
Neitex <neitexplay@yandex.by>
2018-04-10 21:22:35 +0300

    Added Emebed, many commands

--- main2.js
+++ main2.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const ntx = new Discord.Client();
 const prefix = "!";
-const prefixwat = "?";
 const TOKEN = "NDI0NTQyODM4NjUxMzU1MTM4.Daq48w.FzibeFcxfrpy-ulrEizB4sVZaog";
 const bot = ntx;
 //-----------установка констант--------\\
@@ -24,12 +23,33 @@ ntx.on('message', function(message){
 
         switch (command[0].toLowerCase()) {
           case "привет":{
-            message.reply("Привет! Рад видеть тебя на нашем сервере! :wink::wave:")
+            message.reply("Привет! Рад видеть тебя на нашем сервере! :wink::wave:");
             break;
-          }
-
-        }
-
+          } //конец привета
+          case "пока":{
+          message.reply("Пока. Возвращайся скорей :wink:")
+          break;
+          } //конец пока
+          case "тылох": {
+              message.channel.send("ЧЕГО БЛ*ТЬ?");
+              break;
+            } //конец оскорбления
+            case "илита":{
+                var elite = new Discord.RichEmbed()
+              .addField("Паша (Neitex)", "АДМЕН. Создатель этого бота. Могёт.")
+              .addField("Герман (ColdWaker)", "ВИП на сервере. Лучший друг адмена. Могёт.")
+              .addField("Артур (Artur)", "Артур. Рост - over9999.")
+              .addField("Коляныч (NikolaCat)", "Просто коля. Редко играет :cry:")
+              .addField("NTXbot (NTXbot)", "Ну а как же без меня?)")
+              .addField("Арсений (стоп. его тоже нужно вписать?)", "Он ниже меня по рангу)")
+              message.channel.sendEmbed(elite);
+                break;
+              } //конец представления илиты
+            default: {
+              message.channel.sendMessage("Окееей. Я не понял.");
+              break;
+            } // конец дефаулта
+        } 
 
 
 
