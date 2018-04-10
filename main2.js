24d93e25d122d425f94b11c393dd90541991662c
Neitex <neitexplay@yandex.by>
2018-04-10 16:17:51 +0300

    Hello command is rewrited.

--- main2.js
+++ main2.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const ntx = new Discord.Client();
-const prefix = "!" || "?";
+const prefix = "!";
+const prefixwat = "?";
 const TOKEN = "NDI0NTQyODM4NjUxMzU1MTM4.Daq48w.FzibeFcxfrpy-ulrEizB4sVZaog";
 const bot = ntx;
 //-----------установка констант--------\\
@@ -19,7 +20,15 @@ ntx.on('ready', () => {
 ntx.on('message', function(message){
         if(message.author.equals(ntx.user)) return;
         if(!message.content.startsWith(prefix)) return;
-        
+        var command = message.content.substring(prefix.length).split(" ");
+
+        switch (command[0].toLowerCase()) {
+          case "привет":{
+            message.reply("Привет! Рад видеть тебя на нашем сервере! :wink::wave:")
+            break;
+          }
+
+        }
 
 
 
