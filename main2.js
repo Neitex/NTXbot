cda93d0de78057aaed81ef67879ff40e446540a3
Neitex <neitexplay@yandex.by>
2018-04-12 23:12:17 +0300

    Самое большое обновление бота.
    Добавлены комманды: помощь (в разработке), телефоны илиты, добавлен статус.

--- main2.js
+++ main2.js
@@ -3,12 +3,37 @@ const ntx = new Discord.Client();
 const prefix = "!";
 const TOKEN = "NDI0NTQyODM4NjUxMzU1MTM4.Daq48w.FzibeFcxfrpy-ulrEizB4sVZaog";
 const bot = ntx;
+{
+   var elite = new Discord.RichEmbed()
+  .setAuthor("NTXbot: представление элиты")
+  .setColor(0x6fe8dc)
+  .setFooter("Доволен? Теперь ты знаешь всю элиту сервера. Ещё есть !телефоны")
+  .addField("Паша (Neitex)", "АДМЕН. Создатель этого бота. Имеет заниженную самооценку. Могёт.")
+  .addField("Герман (ColdWaker)", "ВИП на сервере. Лучший друг адмена. Могёт.")
+  .addField("Артур (Artur)", "Артур. Рост - over9999.")
+  .addField("Коляныч (NikolaCat)", "Просто коля. Редко играет :cry:")
+  .addField("NTXbot (NTXbot)", "Ну а как же без меня?)")
+  .addField("Арсений (стоп. его тоже нужно вписать?)", "Он ниже меня по рангу)")
+}
+
+  {
+    var phones = new Discord.RichEmbed()
+    .setColor(0x001fff)
+    .setFooter("Ода. Ты можешь им позвонить.")
+    .addField("Паша (Neitex)", "+375 (44) 559-95-01 ")
+    .addField("Герман (ColdWaker)", "+375 (33) 614-53-41 " )
+    .addField("Артур (Artur)", "+375 (29) 333-73-03 (Редко отвечает) ")
+    .addField("Коляныч (NikolaCat)", "Держит его в секрете :shrug:")
+    .addField("NTXbot (NTXbot)", "Я бот так-то. У меня его нет")
+    .addField("Арсений (Не звоните... Не рискуйте)", "+375 (44) 579-64-72 ")
+  }
 //-----------установка констант--------\\
 
 
 //-----------подключаемся к дикорду----\\
 ntx.login(TOKEN);
 ntx.on('ready', () => {
+    ntx.user.setGame("!помощь");
     console.log(`              Скрипт от Neitex'a`);
     console.log(`           Скрипт работает стабильно` );
     console.log(` Скрипт подключён к дискорду как ${ntx.user.tag}!`);
@@ -35,16 +60,17 @@ ntx.on('message', function(message){
               break;
             } //конец оскорбления
             case "илита":{
-                var elite = new Discord.RichEmbed()
-              .addField("Паша (Neitex)", "АДМЕН. Создатель этого бота. Могёт.")
-              .addField("Герман (ColdWaker)", "ВИП на сервере. Лучший друг адмена. Могёт.")
-              .addField("Артур (Artur)", "Артур. Рост - over9999.")
-              .addField("Коляныч (NikolaCat)", "Просто коля. Редко играет :cry:")
-              .addField("NTXbot (NTXbot)", "Ну а как же без меня?)")
-              .addField("Арсений (стоп. его тоже нужно вписать?)", "Он ниже меня по рангу)")
               message.channel.sendEmbed(elite);
                 break;
               } //конец представления илиты
+              case "телефоны": {
+                message.channel.sendEmbed(phones);
+                break;
+              } //конец телефонов
+              case "помощь": {
+                message.author.sendEmbed(phones);
+                break;
+              }
             default: {
               message.channel.sendMessage("Окееей. Я не понял.");
               break;
